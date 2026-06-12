e98bbbb74858679c83532536eabbe7ac0184c9ef
gm3k4g <thingstuffet@gmail.com>
2026-06-12 23:09:05 +0300

    style: show repository URL as plain text field instead of clickable title

--- source/modules/server_start_card.cpp
+++ source/modules/server_start_card.cpp
@@ -20,10 +20,10 @@ public:
         if (ch == 0) return;
         dpp::embed embed;
         embed.set_title(msg_.empty() ? ":green_circle: The server has started" : msg_);
-        embed.set_url(PROJECT_URL);
         embed.set_description(PROJECT_DESCRIPTION);
         embed.add_field("Version", PROJECT_VERSION_MAJOR "." PROJECT_VERSION_MINOR, true);
         embed.add_field("Author", PROJECT_AUTHOR, true);
+        embed.add_field("Repository", PROJECT_URL, false);
         embed.set_color(0x57F287);
         embed.set_timestamp(std::time(nullptr));
         bot.message_create(dpp::message(ch, "").add_embed(embed));
