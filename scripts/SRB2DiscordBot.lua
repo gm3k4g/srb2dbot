a646c99b605b089f29061d3063e2ab5868dde920
gm3k4g <thingstuffet@gmail.com>
2026-06-14 17:12:57 +0300

    Revert "fix: skip join/quit events for console player (node 0) to prevent duplicate embeds"
    This reverts commit daa274958d904272c3a9a697565bf040aac7eb20.

--- scripts/SRB2DiscordBot.lua
+++ scripts/SRB2DiscordBot.lua
@@ -499,15 +499,13 @@ addHook("PlayerThink", function(player)
  then
 		if player.logjoin != true
  then
-			player.logjoin = true
-			if #player != 0 then
-				local text = "[EVENT:CHAT]|["..#player.."]|System|:rocket:"..player.name.." has joined the game:rocket:\n"
-				if text
+			local text = "[EVENT:CHAT]|["..#player.."]|System|:rocket:"..player.name.." has joined the game:rocket:\n"
+			if text
  then
-					DiscordBot.Functions.spamchatbug(player, text, true)
-					DiscordBot.Data.msgsrb2 = DiscordBot.Data.msgsrb2.."[EVENT:PLAYER_JOIN]|"..player.name.."|"..#player.."\n"
-					DiscordBot.Functions.flush_msgsrb2()
-				end
+				DiscordBot.Functions.spamchatbug(player, text, true)
+				DiscordBot.Data.msgsrb2 = DiscordBot.Data.msgsrb2.."[EVENT:PLAYER_JOIN]|"..player.name.."|"..#player.."\n"
+				DiscordBot.Functions.flush_msgsrb2()
+				player.logjoin = true
 			end
 		end
 	end
@@ -537,7 +535,6 @@ addHook("PlayerJoin", function(playernum)
 end)
 
 addHook("PlayerQuit", function(player, reason)
-	if #player == 0 then return end
 	local text = nil
 	if DiscordBot.Commands.cv_joinquit.value == 1
  then
